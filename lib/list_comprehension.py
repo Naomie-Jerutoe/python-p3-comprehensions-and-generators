ce0b8ca8914931fd685d9c94cd5e6a4d99f5a892
Naomi Lagat <naomiejerutoe@gmail.com>
2024-02-29 02:04:16 +0300

    complete task using list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_in_list = [num for num in num_list if num % 2 == 0]
+    return evens_in_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_mark =[sentence + '!' for sentence in sentence_list]
+    return exclamation_mark
